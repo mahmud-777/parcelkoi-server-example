0610a390ba9be5938154dcd5277856fd087c1315
mahmud-777 <mahmud33k@gmail.com>
2022-06-26 09:22:53 +0600

    for testing purpose with jest

--- mongo.js
+++ mongo.js
@@ -1,17 +1,19 @@
 import mongoose from "mongoose";
 
-const uri = "mongodb://localhost:27017/parcelkoi";
-const options = {};
+export const uri = "mongodb://localhost:27017/parcelkoi";
+// const options = { useUnifiedTopology: true };
+const options = { };
 
 const log = (msg) => console.log(msg);
 
-const connectWithDb = () => {
+export const connectWithDb = () => {
     mongoose.connect(uri, options, (err, db) => {
         if(err){
-            console.error(err);
+            // console.error(err);
+            throw err;
         }
-        else log("database connection established");
+        // else log("database connection established");
     })
 }
 
-export default connectWithDb;
+// export default connectWithDb;
